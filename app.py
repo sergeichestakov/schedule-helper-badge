815d1be5e3c2b2b5d2adc4462bfb61c1c52577b3
Sergei Chestakov <schestakov@ucdavis.edu>
2019-10-08 09:47:03 -0700

    Return json object with required fields for badge

--- app.py
+++ app.py
@@ -11,9 +11,16 @@ LISTING_URL = 'https://chrome.google.com/webstore/detail/uc-davis-schedule-helpe
 def get_users():
     page = urlopen(LISTING_URL)
     soup = BeautifulSoup(page, "html.parser")
-    num_users = soup.find('span', {'class': 'e-f-ih'}).text
+    [num_users, _] = soup.find('span', {'class': 'e-f-ih'}).text.split(' ')
 
-    return jsonify(num_users)
+    res = {
+        'schemaVersion': 1,
+        'label': 'Num Users',
+        'message': num_users,
+        'color': 'blue'
+    }
+
+    return jsonify(res)
 
 if __name__ == '__main__':
     app.run()
